08a445fae8ada3eabacaf2a896aac9a19c55572e
elenastamenkovska <elena.stamenkovska@halicea.com>
2025-09-21 10:05:29 +0200

    feat: remove firebase options

--- Frontend/placeful/lib/main.dart
+++ Frontend/placeful/lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:placeful/core/services/service_locatior.dart';
-import 'package:placeful/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
   await Firebase.initializeApp();
   setupLocator();
   runApp(const MyApp());
